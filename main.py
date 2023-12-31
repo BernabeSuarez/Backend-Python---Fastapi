bb740b15526a09c39e46ae871216a59cafb69c89
Bernabe <bernatattoo@gmail.com>
2023-12-31 18:28:14 -0300

    Add static folder

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from routes import user
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
 
 app.include_router(user.router)
+# servir contenido estatico
+app.mount("/static", StaticFiles(directory="static"), name="static")
